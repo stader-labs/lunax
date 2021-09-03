f111afb764ebb0eda02dd89b443f2b065cdd9eb1
Bharath Vedartham <vedabharath12345@gmail.com>
2021-09-03 11:17:15 +0530

    Fix cargo clippy

--- contracts/sic-base/src/msg.rs
+++ contracts/sic-base/src/msg.rs
@@ -1,6 +1,5 @@
 use crate::state::{BatchUndelegationRecord, State};
-use cosmwasm_std::{Addr, Uint128, Binary};
-use cw_storage_plus::U64Key;
+use cosmwasm_std::{Addr, Binary, Uint128};
 use schemars::JsonSchema;
 use serde::{Deserialize, Serialize};
 
--- contracts/sic-base/src/state.rs
+++ contracts/sic-base/src/state.rs
@@ -3,8 +3,6 @@ use serde::{Deserialize, Serialize};
 
 use cosmwasm_std::{Addr, Coin, Decimal, Timestamp, Uint128};
 use cw_storage_plus::{Item, Map, U64Key};
-use std::fmt;
-use std::fmt::Display;
 
 #[derive(Serialize, Deserialize, Clone, Debug, PartialEq, JsonSchema)]
 pub struct State {
