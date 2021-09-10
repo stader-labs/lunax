f99f7d2de4b4fcc71f1797674d062268418e0e4a
Bharath Vedartham <vedabharath12345@gmail.com>
2021-09-10 16:22:46 +0530

    update sic-auto-compound schema

--- contracts/sic-auto-compound/examples/schema.rs
+++ contracts/sic-auto-compound/examples/schema.rs
@@ -3,8 +3,9 @@ use std::fs::create_dir_all;
 
 use cosmwasm_schema::{export_schema, remove_schemas, schema_for};
 
-use sic_stake_max::msg::{CountResponse, ExecuteMsg, InstantiateMsg, QueryMsg};
-use sic_stake_max::state::State;
+use sic_auto_compound::msg::{ExecuteMsg, InstantiateMsg, QueryMsg};
+use sic_auto_compound::state::State;
+use sic_auto_compound::msg::{GetTotalTokensResponse, GetStateResponse, GetFulfillableUndelegatedFundsResponse};
 
 fn main() {
     let mut out_dir = current_dir().unwrap();
@@ -16,5 +17,8 @@ fn main() {
     export_schema(&schema_for!(ExecuteMsg), &out_dir);
     export_schema(&schema_for!(QueryMsg), &out_dir);
     export_schema(&schema_for!(State), &out_dir);
-    export_schema(&schema_for!(CountResponse), &out_dir);
+    export_schema(&schema_for!(GetTotalTokensResponse), &out_dir);
+    export_schema(&schema_for!(GetStateResponse), &out_dir);
+    export_schema(&schema_for!(GetFulfillableUndelegatedFundsResponse), &out_dir);
+
 }
